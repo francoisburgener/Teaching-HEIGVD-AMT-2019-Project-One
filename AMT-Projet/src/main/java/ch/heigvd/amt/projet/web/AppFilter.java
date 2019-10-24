f7da4f445f8c71552257fbdf0d463a3803da77a8
Burgener francois <francois.burgener@hotmail.fr>
2019-10-24 15:08:05 +0200

    Filter work

---
+++ AMT-Projet/src/main/java/ch/heigvd/amt/projet/web/AppFilter.java
@@ -0,0 +1,32 @@
+package ch.heigvd.amt.projet.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/app",filterName = "AppFilter")
+public class AppFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpSession session = request.getSession(false);
+
+        if(session == null || session.getAttribute("userSession") == null){
+            response.sendRedirect("signin");
+        }else{
+            chain.doFilter(req, resp);
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
