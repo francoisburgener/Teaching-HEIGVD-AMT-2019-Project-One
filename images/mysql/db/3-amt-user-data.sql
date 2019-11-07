67a8e307604bdd2b6ac8a7816d04e3f517fff673
Burgener francois <francois.burgener@hotmail.fr>
2019-11-07 09:33:01 +0100

    generate data

---
+++ images/mysql/db/3-amt-user-data.sql
@@ -0,0 +1,15 @@
+USE `amt-db` ;
+
+INSERT INTO User (username,fullname,email,password)VALUES('galiaker','François Burgener','francois.burgener@hotmail.fr','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+INSERT INTO User (username,fullname,email,password)VALUES('jeandupont','Jean Dupont','jean.dupont@hotmail.com','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+INSERT INTO User (username,fullname,email,password)VALUES('jean','Jean','jean@hotmail.com','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+INSERT INTO User (username,fullname,email,password)VALUES('philipe','Philipe','philipe@hotmail.com','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+INSERT INTO User (username,fullname,email,password)VALUES('tiago','Tiago','tiago@hotmail.com','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+INSERT INTO User (username,fullname,email,password)VALUES('albert','einstein','albert@albert.com','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+INSERT INTO User (username,fullname,email,password)VALUES('david','David','david@hotmail.com','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+INSERT INTO User (username,fullname,email,password)VALUES('michel','Michel Michel','michel.michel@hotmail.com','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+INSERT INTO User (username,fullname,email,password)VALUES('alex','Alex','alex@hotmail.com','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+INSERT INTO User (username,fullname,email,password)VALUES('toto','Toto','toto@hotmail.com','$2a$10$FfShhsWbwpfTybBpghXDleSc5JsGK2hw69esil.xdl5Qbujb7fnA.');
+
+
+    
