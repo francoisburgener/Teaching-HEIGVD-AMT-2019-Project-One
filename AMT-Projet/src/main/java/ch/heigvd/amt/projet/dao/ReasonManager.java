a83681e45fb5ecd4993b565de7f76e60fa19ecdb
Tiago Póvoa <tiago.quin@gmail.com>
2019-11-10 21:17:10 +0100

    add reason DAO

--- AMT-Projet/src/main/java/ch/heigvd/amt/projet/dao/ReasonManager.java
+++ AMT-Projet/src/main/java/ch/heigvd/amt/projet/dao/ReasonManager.java
@@ -5,8 +5,14 @@ import ch.heigvd.amt.projet.model.Reason;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Stateless
 public class ReasonManager implements ReasonManagerLocal {
@@ -16,8 +22,23 @@ public class ReasonManager implements ReasonManagerLocal {
 
     @Override
     public List<Reason> findAllReasons() {
-        List<Reason> reasons = new ArrayList<>();
+        List<Reason> reasons = new ArrayList<>(); //todo
 
-        return null;
+        try {
+            Connection connection = dataSource.getConnection();
+
+            PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM Reason;");
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()){
+                int id = rs.getInt("idReason");
+                String name = rs.getString("name");
+                reasons.add(Reason.builder().id(id).name(name).build());
+            }
+            connection.close();
+        }catch (SQLException ex){
+            Logger.getLogger(UsersManager.class.getName()).log(Level.SEVERE,null,ex);
+        }
+        return reasons;
     }
 }
